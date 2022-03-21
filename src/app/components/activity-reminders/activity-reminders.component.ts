f8fca6640f71c2f72f2eb1a0052ef93fd9f9903d
Ayala-Cohen <ayalula2000@gmail.com>
2022-03-21 12:24:15 +0200

    Active Reminders

--- src/app/components/activity-reminders/activity-reminders.component.ts
+++ src/app/components/activity-reminders/activity-reminders.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { activityReminders } from 'src/app/classes/ActivityReminders';
+import { RemindersService } from 'src/app/Services/reminders.service';
+import { UserService } from 'src/app/Services/user.service';
 
 export class PeriodicElement {
   name: string;
@@ -26,18 +30,42 @@ export class ActivityRemindersComponent implements OnInit {
   // ];
   // displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   // dataSource = this.ELEMENT_DATA;
-  ListActivityReminders:object[];
-  
+  ListActivityReminders: activityReminders[];
 
-  constructor() { }
+
+  constructor(private reminderServe: RemindersService, private userServe: UserService, private router:Router) { }
 
   ngOnInit(): void {
+    this.reminderServe.GetActivityRemindersByGmail(this.userServe.currentuser.gmail).subscribe(x => {
+      this.ListActivityReminders = x
+    })
+  }
+  getNextTime() {
+    let next_time = this.ListActivityReminders[0].NextTakingTime
+    let min_difference = 0
+    let currentDateTime = new Date()
+    for (let activeReminder of this.ListActivityReminders) {
+      let current_diff = (currentDateTime.getHours() - activeReminder.NextTakingTime.getHours())
+      if (current_diff < min_difference) {
+        min_difference = current_diff
+        next_time = activeReminder.NextTakingTime
+      }
+    }
+    return next_time
+  }
+
+  showDetails(activeReminder:activityReminders){
+    this.reminderServe.medicineTakeDetailsForm.get("namemedicine").setValue(activeReminder.MedicineName)
+    this.reminderServe.medicineTakeDetailsForm.get("namepatient").setValue(this.userServe.currentuser.fname)
+    this.reminderServe.medicineTakeDetailsForm.get("frequency").setValue(this.userServe.currentuser.fname) //change
+    this.reminderServe.medicineTakeDetailsForm.get("leftdate").setValue(activeReminder.LeftDays)
+    this.reminderServe.medicineTakeDetailsForm.get("remarks").setValue(activeReminder.LeftDays) //change - what is remark
+    this.router.navigate(["detailsMedicineTake"])
   }
 
 
 
 
-  
 }
 
 
